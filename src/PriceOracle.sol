12f1ec5eb210939cd95f3d0289ee4610d932cd8f
Jon Bray <jon@heyjonbray.com>
2024-08-07 15:16:47 -0400

    feat: add testnet PriceOracle.sol contract

---
+++ src/PriceOracle.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/*
+ * @title Xeon Price Oracle (Testnet)
+ * @author Jon Bray <jon@xeon-protocol.io>
+ * @notice this is a testnet price oracle that allows for manual price changes
+ */
+contract PriceOracle is Ownable {
+    //==================== STATE VARIABLES ====================//
+    address public constant oVELA = 0xb7E16D46f26B1615Dcc501931F28F07fD4b0D7F4;
+    address public constant oPEPE = 0x7dC9ecE25dcCA41D8a627cb47ded4a9322f7722b;
+    address public constant oDEGEN = 0x9B9852A943a570685c3704d70C4F1ebD5EdE109B;
+    address public constant oHIGHER = 0x9855d38b7E6270B9f22F283A0C62330b16Ac909C;
+    address public constant oROR = 0xEb2DCAFFFf1b0d5BA76F14Fe6bB8348126339FcB;
+    address public constant WETH = 0x395cB7753B02A15ed1C099DFc36bF00171F18218;
+
+    //==================== MAPPINGS ====================//
+    mapping(address => uint256) private tokenPrices;
+    mapping(address => bool) public admins;
+
+    //===================== EVENTS =====================//
+    event PriceUpdated(address indexed token, uint256 price);
+    event AdminAdded(address indexed admin);
+    event AdminRemoved(address indexed admin);
+
+    //================== CONSTRUCTOR ==================//
+    constructor() Ownable(msg.sender) {
+        tokenPrices[oVELA] = 0;
+        tokenPrices[oPEPE] = 0;
+        tokenPrices[oDEGEN] = 0;
+        tokenPrices[oHIGHER] = 0;
+        tokenPrices[oROR] = 0;
+    }
+
+    //================ MODIFIERS =================//
+    modifier onlyAdmin() {
+        require(admins[msg.sender] || owner() == msg.sender, "Caller is not an admin");
+        _;
+    }
+
+    //================ EXTERNAL FUNCTIONS ================//
+    function addAdmin(address admin) external onlyOwner {
+        admins[admin] = true;
+        emit AdminAdded(admin);
+    }
+
+    function removeAdmin(address admin) external onlyOwner {
+        admins[admin] = false;
+        emit AdminRemoved(admin);
+    }
+
+    /**
+     * @notice Sets the price of a token in WETH
+     * @param token The address of the token
+     * @param priceInWETH The price of the token in WETH
+     */
+    function setWETHPrice(address token, uint256 priceInWETH) external onlyAdmin {
+        tokenPrices[token] = priceInWETH;
+        emit PriceUpdated(token, priceInWETH);
+    }
+
+    /**
+     * @notice Sets the price of WETH in USD
+     * @param priceInUSD The price of WETH in USD
+     */
+    function setWETHPriceInUSD(uint256 priceInUSD) external onlyAdmin {
+        tokenPrices[WETH] = priceInUSD;
+        emit PriceUpdated(WETH, priceInUSD);
+    }
+
+    //==================== GETTERS ====================//
+    function getAdminStatus(address admin) external view returns (bool) {
+        return admins[admin];
+    }
+
+    function getValueInWETH(address token) external view returns (uint256) {
+        return tokenPrices[token];
+    }
+}
