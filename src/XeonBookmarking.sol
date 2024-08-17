b9e977bc59050a00bdcc0e9e10ec22449204b49b
Jon Bray <jon@heyjonbray.com>
2024-08-17 05:21:42 -0400

    feat: use chainId in XeonBookmarks

--- src/XeonBookmarking.sol
+++ src/XeonBookmarking.sol
@@ -7,86 +7,101 @@ pragma solidity 0.8.20;
  * @dev this contract allows users to bookmark hedging options
  * it provides functions to toggle bookmarks, check bookmark status,
  * and retrieve all bookmarks for a user
+ *
+ * @notice this contract handles bookmarks for options across all chains
+ * by using a unique composite key
  */
 contract XeonBookmarking {
     //=============== MAPPINGS ===============//
-    // mapping of bookmark status by user and dealId
-    mapping(address => mapping(uint256 => bool)) internal bookmarks;
-    // mapping of bookmark index by user and dealId
-    mapping(address => mapping(uint256 => uint256)) internal bookmarkIndex;
-    // mapping of bookmarks for a user by dealId
-    mapping(address => uint256[]) internal bookmarkedOptions;
+    // mapping of bookmark status by [user][chainId][dealId]
+    mapping(address => mapping(uint256 => mapping(uint256 => bool))) internal bookmarks;
+
+    // mapping of bookmarked dealIds by [user][chainId]
+    mapping(address => mapping(uint256 => uint256[])) internal bookmarkedOptions;
+
+    // mapping of index of each bookmarked dealId in the bookmarkedOptions array
+    mapping(address => mapping(uint256 => mapping(uint256 => uint256))) internal bookmarkIndex;
 
     //=============== EVENTS ===============//
-    event BookmarkAdded(address indexed user, uint256 hedgeId);
-    event BookmarkRemoved(address indexed user, uint256 hedgeId);
+    event BookmarkAdded(address indexed user, uint256 indexed chainId, uint256 indexed dealId);
+    event BookmarkRemoved(address indexed user, uint256 indexed chainId, uint256 indexed dealId);
 
     //=============== FUNCTIONS ===============//
-
     /**
-     * @notice add bookmark for a hedging option using dealId
+     * @notice adds a bookmark for a hedging option
      *
-     * this function adds a bookmark for a hedging option based on its dealId for the caller
+     * this function adds a bookmark for a hedging option based on its chainId and dealId
      * emits an event indicating the bookmark was added
      *
-     * @param _dealId unique identifier of the hedging option
+     * @param chainId ID of the chain
+     * @param dealId unique identifier of the hedging option
      */
-    function addBookmark(uint256 _dealId) external {
-        require(!bookmarks[msg.sender][_dealId], "Bookmark already exists");
+    function addBookmark(uint256 chainId, uint256 dealId) external {
+        require(!bookmarks[msg.sender][chainId][dealId], "Bookmark already exists");
 
-        // Add the bookmark
-        bookmarkedOptions[msg.sender].push(_dealId);
-        bookmarks[msg.sender][_dealId] = true;
-        bookmarkIndex[msg.sender][_dealId] = bookmarkedOptions[msg.sender].length - 1;
+        // add the bookmark
+        bookmarks[msg.sender][chainId][dealId] = true;
+        bookmarkedOptions[msg.sender][chainId].push(dealId);
+        bookmarkIndex[msg.sender][chainId][dealId] = bookmarkedOptions[msg.sender][chainId].length - 1;
 
-        emit BookmarkAdded(msg.sender, _dealId);
+        emit BookmarkAdded(msg.sender, chainId, dealId);
     }
 
     /**
-     * @notice removes a bookmark for a hedging option using its ID
+     * @notice removes a bookmark for a hedging option
      *
-     * this function removes a bookmark for a hedging option based on its dealId for the caller
+     * this function removes a bookmark for a hedging option based on its chainId and dealId
      * emits an event indicating the bookmark was removed
      *
-     * @param _dealId unique identifier of the hedging option
+     * @param chainId ID of the chain
+     * @param dealId unique identifier of the hedging option
      */
-    function removeBookmark(uint256 _dealId) external {
-        require(bookmarks[msg.sender][_dealId], "Bookmark does not exist");
+    function removeBookmark(uint256 chainId, uint256 dealId) external {
+        require(bookmarks[msg.sender][chainId][dealId], "Bookmark does not exist");
 
-        // Remove the bookmark
-        uint256 index = bookmarkIndex[msg.sender][_dealId];
-        uint256 lastIndex = bookmarkedOptions[msg.sender].length - 1;
-        uint256 lastDealId = bookmarkedOptions[msg.sender][lastIndex];
+        // get the index of the dealId to remove
+        uint256 index = bookmarkIndex[msg.sender][chainId][dealId];
+        uint256 lastIndex = bookmarkedOptions[msg.sender][chainId].length - 1;
+        uint256 lastDealId = bookmarkedOptions[msg.sender][chainId][lastIndex];
 
-        // Swap and pop
-        bookmarkedOptions[msg.sender][index] = lastDealId;
-        bookmarkIndex[msg.sender][lastDealId] = index;
+        // swap the last dealId with the one to remove
+        bookmarkedOptions[msg.sender][chainId][index] = lastDealId;
+        bookmarkIndex[msg.sender][chainId][lastDealId] = index;
 
-        bookmarkedOptions[msg.sender].pop();
-        delete bookmarks[msg.sender][_dealId];
-        delete bookmarkIndex[msg.sender][_dealId];
+        // remove the last element
+        bookmarkedOptions[msg.sender][chainId].pop();
 
-        emit BookmarkRemoved(msg.sender, _dealId);
+        // cleanup the mappings
+        delete bookmarks[msg.sender][chainId][dealId];
+        delete bookmarkIndex[msg.sender][chainId][dealId];
+
+        emit BookmarkRemoved(msg.sender, chainId, dealId);
     }
 
     /**
-     * @notice get the bookmark status of a hedging option for a specific user.
+     * @notice gets the bookmark status of a hedging option for a specific user
+     *
+     * this function retrieves the bookmark status of a hedging option for a specific user
      *
      * @param user address of the user
-     * @param _dealId unique identifier of the hedging option
+     * @param chainId ID of the chain
+     * @param dealId unique identifier of the hedging option
      * @return bookmark status
      */
-    function getBookmark(address user, uint256 _dealId) public view returns (bool) {
-        return bookmarks[user][_dealId];
+    function getBookmark(address user, uint256 chainId, uint256 dealId) public view returns (bool) {
+        return bookmarks[user][chainId][dealId];
     }
 
     /**
-     * @notice get all bookmarks of a user
+     * @notice gets all bookmarks of a user for a specific chain
      *
-     * @param user address of the user.
-     * @return array containing all bookmarked hedging option IDs
+     * this function retrieves all bookmarks of a user for a specific chainId
+     *
+     * @param user address of the user
+     * @param chainId ID of the chain
+     * @return array containing all bookmarked hedging option IDs for the specified chain
      */
-    function getUserBookmarks(address user) public view returns (uint256[] memory) {
-        return bookmarkedOptions[user];
+    function getUserBookmarks(address user, uint256 chainId) public view returns (uint256[] memory) {
+        return bookmarkedOptions[user][chainId];
     }
 }
